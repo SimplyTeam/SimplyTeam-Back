9c8c0629af6d7cc079a73ce5f34e4f09ae01a2c1
Tim GIMENEZ <tim.gimenez26@gmail.com>
2023-03-07 14:00:01 +0100

    feat: tests login with valid credential

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -28,9 +28,8 @@ class AuthTest extends TestCase
 
         $response->assertStatus(201)
                  ->assertJsonStructure([
-                     'access_token',
-                     'token_type',
-                     'expires_in',
+                     'user',
+                     'access_token'
                  ]);
     }
 
@@ -67,18 +66,75 @@ class AuthTest extends TestCase
             ->assertJsonValidationErrors('email');
     }
 
-    public function test_register_user_with_short_password()
+    public function test_register_user_with_invalid_passwords()
     {
+        $invalidPasswords = [
+            'password',         // ne contient pas de caractère spécial
+            'Password',         // ne contient pas de chiffre
+            'password1',        // ne contient pas de caractère spécial en plus du chiffre
+            'Password!',        // ne contient pas de minuscule
+            'password!',        // ne contient pas de majuscule
+            'PASSWORD!',        // ne contient pas de minuscule
+            'Password1',        // ne contient pas de caractère spécial
+            'Pa!3wor',             // trop court (moins de 8 caractères)
+        ];
+
+        foreach ($invalidPasswords as $password) {
+            $response = $this->postJson('/api/register', [
+                'name' => $this->faker->name(),
+                'email' => $this->faker->email(),
+                'password' => $password,
+                'password_confirmation' => $password,
+            ]);
+
+            $response
+                ->assertStatus(422)
+                ->assertJsonValidationErrors('password');
+        }
+    }
+
+    function test_login_doesn_t_work_with_invalid_credential(){
+        $response = $this->postJson('/api/login', [
+            'email' => $this->faker->email(),
+            'password' => $this->faker->password()
+        ]);
+
+        $response
+            ->assertStatus(422)
+            ->assertJsonValidationErrors('message');
+    }
+
+    function test_login_work_with_valid_credential(){
+        $email = $this->faker->unique()->safeEmail();
+        $password = "passW0rd!123";
+
+        var_dump($password);
+
         $data = [
             'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'password' => 'short',
-            'password_confirmation' => 'short',
+            'email' => $email,
+            'password' => $password,
+            'password_confirmation' => $password,
         ];
 
         $response = $this->postJson('/api/register', $data);
 
-        $response->assertStatus(422)
-            ->assertJsonValidationErrors('password');
+        $response->assertStatus(201)
+            ->assertJsonStructure([
+                'user',
+                'access_token'
+            ]);
+
+        $response = $this->postJson('/api/login', [
+            'email' => $email,
+            'password' => $password
+        ]);
+
+        $response
+            ->assertStatus(201)
+            ->assertJsonStructure([
+                'user',
+                'access_token'
+            ]);
     }
 }
