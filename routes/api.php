927d7922d3824eb846d8a04d661dcee9c684a350
Tim GIMENEZ <tim.gimenez26@gmail.com>
2023-03-09 16:02:10 +0100

    feat: auth via google

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\GoogleAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('auth/google', [GoogleAuthController::class, 'redirectToGoogle'])
+    ->name('google.redirect')
+    ->middleware('web', 'Illuminate\Session\Middleware\StartSession');
+Route::get('/auth/google/callback', [GoogleAuthController::class, 'handleGoogleCallback'])->name('google.callback');
 
 Route::middleware(['auth:api'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
