b1d243c32876cca7aa0e91c2ad12c2d23536bf1b
Tim GIMENEZ <tim.gimenez26@gmail.com>
2023-03-30 17:24:01 +0200

    fix: user data return

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -11,10 +11,8 @@ class UserResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'first_name' => $this->first_name,
-            'last_name' => $this->last_name,
+            'name' => $this->name,
             'email' => $this->email,
-            'phone' => $this->phone,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at
         ];
