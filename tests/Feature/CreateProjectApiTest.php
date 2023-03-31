5e94a1624df0b5a50f26699c53859aded1347d73
Tim GIMENEZ <tim.gimenez26@gmail.com>
2023-03-31 12:24:07 +0200

    refacto: rename test ProjectsApiTest -> CreateProjectApiTest

--- tests/Feature/CreateProjectApiTest.php
+++ tests/Feature/CreateProjectApiTest.php
@@ -19,7 +19,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 use App\Models\User;
 
-class ProjectsApiTest extends TestCase
+class CreateProjectsApiTest extends TestCase
 {
 
     protected function setUp(): void
