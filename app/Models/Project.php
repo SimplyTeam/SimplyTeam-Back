786e1187f0d6b0b847d5170919c9e81c2fd6e903
Tim GIMENEZ <timgim2000@hotmail.fr>
2023-06-06 23:17:37 +0200

    refacto: update project model file format

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -19,7 +19,8 @@ class Project extends Model
         return $this->belongsTo(Workspace::class);
     }
 
-    public function sprints() {
+    public function sprints()
+    {
         return $this->hasMany(Sprint::class);
     }
 }
