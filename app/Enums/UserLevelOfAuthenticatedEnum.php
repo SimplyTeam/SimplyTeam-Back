96ce1026d97d67c77e127e06c35c3480d31a0691
Tim GIMENEZ <tim.gimenez26@gmail.com>
2023-07-05 16:36:05 +0200

    feat: StatusLevelOfAuthenticatedUserEnum

---
+++ app/Enums/UserLevelOfAuthenticatedEnum.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+/**
+ * @method static StatusLevelOfAuthenticatedEnum PASSED()
+ * @method static StatusLevelOfAuthenticatedEnum CURRENT()
+ * @method static StatusLevelOfAuthenticatedEnum FUTURE()
+ */
+class StatusLevelOfAuthenticatedEnum
+{
+    private const PASSED = 'passed';
+    private const CURRENT = 'current';
+    private const FUTURE = 'future';
+}
