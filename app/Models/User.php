85288e678c53f8811fe3219b562e3e6e90ef9f65
Tim GIMENEZ <tim.gimenez26@gmail.com>
2023-07-05 16:36:38 +0200

    feat: create getAuthenticatedUser method on User class

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
@@ -41,6 +42,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public static function getAuthenticatedUser()
+    {
+        if (Auth::check()) {
+            return Auth::user();
+        }
+        return null;
+    }
 
     public function workspaces()
     {
