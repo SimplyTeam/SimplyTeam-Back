363b7608e4b7bbe6de160b0178c3f04ac02468dc
Tim GIMENEZ <timgim2000@hotmail.fr>
2023-08-21 14:41:19 +0200

    feat: create auth controller documentation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,8 +8,35 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
+
+/**
+ * @OA\Tag(
+ *     name="Authentication",
+ *     description="API Endpoints for Authentication"
+ * )
+ */
 class AuthController extends Controller
 {
+
+    /**
+     * @OA\Post(
+     *     path="/register",
+     *     tags={"Authentication"},
+     *     summary="Register a new user",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(ref="#/components/schemas/UserRegistrationRequest")
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="User registered successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="user", type="object"),
+     *             @OA\Property(property="access_token", type="string")
+     *         )
+     *     )
+     * )
+     */
     public function register(UserRegistrationRequest $request): \Illuminate\Foundation\Application|\Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
     {
         $validatedData = $request->validated();
@@ -23,6 +50,36 @@ class AuthController extends Controller
         return response(['user' => $user, 'access_token' => $accessToken], 201);
     }
 
+
+
+    /**
+     * @OA\Post(
+     *     path="/login",
+     *     tags={"Authentication"},
+     *     summary="Log in a user",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(ref="#/components/schemas/UserLoginRequest")
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="User logged in successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="user", type="object"),
+     *             @OA\Property(property="access_token", type="string")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Invalid credentials",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="errors", type="object",
+     *                 @OA\Property(property="message", type="string", example="Invalid credentials")
+     *             )
+     *         )
+     *     )
+     * )
+     */
     public function login(UserLoginRequest $request): \Illuminate\Foundation\Application|\Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
     {
         $loginData = $request->validated();
@@ -38,6 +95,20 @@ class AuthController extends Controller
         return response(['user' => $user, 'access_token' => $accessToken], 201);
     }
 
+    /**
+     * @OA\Get(
+     *     path="/me",
+     *     tags={"Authentication"},
+     *     summary="Get the authenticated user's data",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Current user's data",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="user", type="object")
+     *         )
+     *     )
+     * )
+     */
     public function me(Request $request): \Illuminate\Foundation\Application|\Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
     {
         $user = $request->user();
@@ -45,6 +116,20 @@ class AuthController extends Controller
         return response(['user' => $user], 200);
     }
 
+    /**
+     * @OA\Post(
+     *     path="/logout",
+     *     tags={"Authentication"},
+     *     summary="Log out the authenticated user",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successfully logged out",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Successfully logged out")
+     *         )
+     *     )
+     * )
+     */
     public function logout(Request $request): \Illuminate\Foundation\Application|\Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
     {
         $request->user()->token()->revoke();
