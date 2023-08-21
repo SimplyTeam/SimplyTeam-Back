0d8fde64fd31e202ad4bf4adf15a1081b7489574
Tim GIMENEZ <timgim2000@hotmail.fr>
2023-08-21 14:49:51 +0200

    feat: create quest controller documentation

--- app/Http/Controllers/QuestController.php
+++ app/Http/Controllers/QuestController.php
@@ -9,8 +9,50 @@ use App\Models\UserQuest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+/**
+ * @OA\Tag(
+ *     name="Quests",
+ *     description="API Endpoints for Managing Quests"
+ * )
+ */
 class QuestController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/quests",
+     *     tags={"Quests"},
+     *     summary="Retrieve a list of quests for the authenticated user based on various query parameters",
+     *     @OA\Parameter(
+     *         name="quest_type",
+     *         in="query",
+     *         required=false,
+     *         description="Filter quests by quest type ID",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Parameter(
+     *         name="default_order",
+     *         in="query",
+     *         required=false,
+     *         description="Order by completion ratio (asc or desc)",
+     *         @OA\Schema(type="string", enum={"asc", "desc"})
+     *     ),
+     *     @OA\Parameter(
+     *         name="in_progress_only",
+     *         in="query",
+     *         required=false,
+     *         description="Filter quests that are in progress only",
+     *         @OA\Schema(type="boolean")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="List of quests for the authenticated user",
+     *         @OA\JsonContent(type="array", @OA\Items(ref="#/components/schemas/Quest"))
+     *     ),
+     *     security={
+     *         {"bearerAuth": {}}
+     *     }
+     * )
+     */
     public function index(GetQuestControllerRequest $request) {
         $user = $request->user();
 
