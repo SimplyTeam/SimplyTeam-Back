0dd15d4aba3bc5dc2bb88a3134472879227cebc9
Tim GIMENEZ <timgim2000@hotmail.fr>
2023-08-22 16:48:01 +0200

    feat: create workspace swagger doc

--- app/Http/Resources/WorkspaceCollection.php
+++ app/Http/Resources/WorkspaceCollection.php
@@ -5,6 +5,19 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
+/**
+ * @OA\Schema(
+ *     schema="WorkspaceCollection",
+ *     description="Collection of Workspaces",
+ *     title="WorkspaceCollection",
+ *     @OA\Property(
+ *         property="workspaces",
+ *         type="array",
+ *         description="Array of workspaces",
+ *         @OA\Items(ref="#/components/schemas/WorkspaceResource")
+ *     )
+ * )
+ */
 class WorkspaceCollection extends ResourceCollection
 {
     public static $wrap = null;
--- app/Http/Resources/WorkspaceResource.php
+++ app/Http/Resources/WorkspaceResource.php
@@ -6,6 +6,61 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @OA\Schema(
+ *     schema="WorkspaceResource",
+ *     description="Workspace Resource representation",
+ *     title="WorkspaceResource",
+ *     required={"id", "name", "description", "created_at", "updated_at"},
+ *     @OA\Property(
+ *         property="id",
+ *         type="integer",
+ *         description="Unique identifier for the Workspace",
+ *         example=1
+ *     ),
+ *     @OA\Property(
+ *         property="name",
+ *         type="string",
+ *         description="Name of the Workspace",
+ *         example="My Workspace"
+ *     ),
+ *     @OA\Property(
+ *         property="description",
+ *         type="string",
+ *         description="Description of the Workspace",
+ *         example="A brief description about the workspace."
+ *     ),
+ *     @OA\Property(
+ *         property="created_at",
+ *         type="string",
+ *         format="date-time",
+ *         description="Time when the Workspace was created"
+ *     ),
+ *     @OA\Property(
+ *         property="updated_at",
+ *         type="string",
+ *         format="date-time",
+ *         description="Last time the Workspace was updated"
+ *     ),
+ *     @OA\Property(
+ *         property="created_by",
+ *         ref="#/components/schemas/UserResource",
+ *         description="User who created the Workspace"
+ *     ),
+ *     @OA\Property(
+ *         property="users",
+ *         type="array",
+ *         description="List of Users associated with the Workspace",
+ *         @OA\Items(ref="#/components/schemas/UserResource")
+ *     ),
+ *     @OA\Property(
+ *         property="projects",
+ *         type="array",
+ *         description="List of Projects under the Workspace",
+ *         @OA\Items(ref="#/components/schemas/ProjectResource")
+ *     )
+ * )
+ */
 class WorkspaceResource extends JsonResource
 {
     public static $wrap = null;
