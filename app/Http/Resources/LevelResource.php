2f1271e62c0cb0bf06ac2b01d30659b447e33ca5
Tim GIMENEZ <timgim2000@hotmail.fr>
2023-08-22 16:41:50 +0200

    feat: swagger level resource

--- app/Http/Resources/LevelResource.php
+++ app/Http/Resources/LevelResource.php
@@ -5,6 +5,38 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @OA\Schema(
+ *     schema="LevelResource",
+ *     description="Level Resource representation",
+ *     title="LevelResource",
+ *     required={"id", "max_point", "min_point", "status"},
+ *     @OA\Property(
+ *         property="id",
+ *         type="integer",
+ *         description="Unique identifier for the Level",
+ *         example=1
+ *     ),
+ *     @OA\Property(
+ *         property="max_point",
+ *         type="integer",
+ *         description="Maximum points for the Level",
+ *         example=100
+ *     ),
+ *     @OA\Property(
+ *         property="min_point",
+ *         type="integer",
+ *         description="Minimum points required for the Level",
+ *         example=0
+ *     ),
+ *     @OA\Property(
+ *         property="status",
+ *         type="string",
+ *         description="Status of the Level for the currently authenticated user",
+ *         example="active"
+ *     )
+ * )
+ */
 class LevelResource extends JsonResource
 {
     public function toArray($request)
