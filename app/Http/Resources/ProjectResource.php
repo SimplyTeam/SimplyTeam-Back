b1858bfa7e6b9896e1dc4c0497bf5425778fd949
Tim GIMENEZ <timgim2000@hotmail.fr>
2023-08-22 16:43:36 +0200

    feat: swagger doc of project resource

--- app/Http/Resources/ProjectResource.php
+++ app/Http/Resources/ProjectResource.php
@@ -5,6 +5,44 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @OA\Schema(
+ *     schema="ProjectResource",
+ *     description="Project Resource representation",
+ *     title="ProjectResource",
+ *     required={"id", "name", "workspace_id", "created_at", "updated_at"},
+ *     @OA\Property(
+ *         property="id",
+ *         type="integer",
+ *         description="Unique identifier for the Project",
+ *         example=1
+ *     ),
+ *     @OA\Property(
+ *         property="name",
+ *         type="string",
+ *         description="Name of the Project",
+ *         example="My Project"
+ *     ),
+ *     @OA\Property(
+ *         property="workspace_id",
+ *         type="integer",
+ *         description="ID of the associated Workspace",
+ *         example=2
+ *     ),
+ *     @OA\Property(
+ *         property="created_at",
+ *         type="string",
+ *         format="date-time",
+ *         description="Time when the Project was created"
+ *     ),
+ *     @OA\Property(
+ *         property="updated_at",
+ *         type="string",
+ *         format="date-time",
+ *         description="Last time the Project was updated"
+ *     )
+ * )
+ */
 class ProjectResource extends JsonResource
 {
     public function toArray($request)
