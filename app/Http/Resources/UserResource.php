7699b3be2ae0d0abfa3f4d363c1b2ccd58911389
Tim GIMENEZ <timgim2000@hotmail.fr>
2023-08-22 16:44:52 +0200

    feat: swagger doc for user resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -5,6 +5,51 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @OA\Schema(
+ *     schema="UserResource",
+ *     description="User Resource representation",
+ *     title="UserResource",
+ *     required={"id", "name", "email", "level", "created_at", "updated_at"},
+ *     @OA\Property(
+ *         property="id",
+ *         type="integer",
+ *         description="Unique identifier for the User",
+ *         example=1
+ *     ),
+ *     @OA\Property(
+ *         property="name",
+ *         type="string",
+ *         description="Name of the User",
+ *         example="John Doe"
+ *     ),
+ *     @OA\Property(
+ *         property="email",
+ *         type="string",
+ *         format="email",
+ *         description="Email address of the User",
+ *         example="john.doe@example.com"
+ *     ),
+ *     @OA\Property(
+ *         property="level",
+ *         type="integer",
+ *         description="Level of the User",
+ *         example=3
+ *     ),
+ *     @OA\Property(
+ *         property="created_at",
+ *         type="string",
+ *         format="date-time",
+ *         description="Time when the User was created"
+ *     ),
+ *     @OA\Property(
+ *         property="updated_at",
+ *         type="string",
+ *         format="date-time",
+ *         description="Last time the User was updated"
+ *     )
+ * )
+ */
 class UserResource extends JsonResource
 {
     public static $wrap = null;
