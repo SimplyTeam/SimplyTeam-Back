861262a39f27ec58fb1a36ad3765921585933011
Tim GIMENEZ <timgim2000@hotmail.fr>
2023-09-11 22:11:08 +0200

    feat: test workspaces

--- app/Http/Controllers/WorkspaceApiController.php
+++ app/Http/Controllers/WorkspaceApiController.php
@@ -20,8 +20,8 @@ class WorkspaceApiController extends Controller
     {
         try {
             $user = $request->user();
-            return response()->json(['OK' => $user->workspaces()]);
-            $workspaces = $user->workspaces()->orderBy('updated_at', 'desc')->get();
+            return response()->json(['OK' => $user->workspaces]);
+            $workspaces = $user->workspaces->orderBy('updated_at', 'desc')->get();
 
             return new WorkspaceCollection($workspaces);
         }catch (Exception $e) {
